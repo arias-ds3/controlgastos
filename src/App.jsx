bc37c67e1188d1938e6f1df21cf8e161f3a86da1
arias-ds3 <ariasfa7@gmail.com>
2026-03-07 10:19:55 -0300

    primera parte hasta login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from "react";
 import { Light, Dark, AuthContextProvider } from "./index";
 import { ThemeProvider } from "styled-components";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {styled} from "styled-components";
 export const ThemeContext = createContext(null);
 function App() {
   const [theme, setTheme] = useState("light");
@@ -13,7 +14,13 @@ function App() {
       <ThemeContext.Provider value={{ setTheme, theme }}>
         <ThemeProvider theme={themeStyle}>
           <AuthContextProvider>
-            <MyRoutes />
+            <Container>
+
+              <Containerbody>
+                <MyRoutes />
+              </Containerbody>
+            </Container>
+            
             <ReactQueryDevtools initialIsOpen={true} />
           </AuthContextProvider>
         </ThemeProvider>
@@ -22,4 +29,12 @@ function App() {
   );
 }
 
+const Container = styled.div`
+display: grid;
+grid-template-columns: 65px 1fr;
+`;
+const Containerbody = styled.div`
+grid-column: 2;
+width: 100%;
+`;
 export default App;
